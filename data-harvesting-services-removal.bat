6097a8b2512f9d9fb4b6510e3b8f0954a8226916
ian <Ian2020@users.noreply.github.com>
2015-08-21 19:52:28 +0100

    Correct service name

--- data-harvesting-services-removal.bat
+++ data-harvesting-services-removal.bat
@@ -8,9 +8,9 @@ REM Run it as an administrator from an elevated command prompt.
 REM It deletes services that Microsoft uses to harvest some of your data.
 
 net stop DiagTrack
-net stop dmwappushsvc
+net stop dmwappushservice
 net stop Wecsvc
-sc delete dmwappushsvc
+sc delete dmwappushservice
 sc delete diagtrack
 sc delete Wecsvc
 cd c:\ProgramData\Microsoft\Diagnosis\ETLLogs\Autologger
