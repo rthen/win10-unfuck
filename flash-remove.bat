ec183ae11c19605498b0289d651674bbe78b8c10
deycrypt <dfd78b57@opayq.com>
2015-10-18 15:18:51 +0200

    Remove other flash files
    The icons in control panel for managing Adobe Flash request.
    @dfkt see if these works. If yes allow commit.

--- flash-remove.bat
+++ flash-remove.bat
@@ -1,8 +1,8 @@
 @echo off
 
-REM original findings by https://twitter.com/thisisdey/status/628914599706914816
+REM original findings by https://twitter.com/deycrypt/status/628914599706914816
 
-echo FUCK OFF FLASH
+echo Die Flash Die!
 echo.
 echo This will remove the baked-in Adobe Flash from Windows 10.
 echo Run this script as admin.
@@ -17,6 +17,14 @@ echo.
 takeown /f "%windir%\SysWOW64\Macromed" /r /d y
 icacls "%windir%\SysWOW64\Macromed" /grant administrators:F /t
 rd /s /q "%windir%\SysWOW64\Macromed"
+echo.
+
+takeown /f "%windir%\SysWOW64\FlashPlayerApp.exe" /r /d y
+icacls "%windir%\SysWOW64\FlashPlayerApp.exe" /grant administrators:F /t
+rd /s /q "%windir%\SysWOW64\FlashPlayerApp.exe"
+takeown /f "%windir%\SysWOW64\FlashPlayerCPLApp.cpl" /r /d y
+icacls "%windir%\SysWOW64\FlashPlayerCPLApp.cpl" /grant administrators:F /t
+rd /s /q "%windir%\SysWOW64\FlashPlayerCPLApp.cpl"
 echo.
 
 rd /s /q "%appdata%\Adobe"
